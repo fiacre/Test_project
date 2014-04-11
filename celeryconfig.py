91f4fc9838dfcc98dd50ed98fbadec12cd9831a4
Andrew Lee <fiacre.patrick@gmail.com>
2014-04-10 21:32:36 -0400

    start creating celery tasks

--- celeryconfig.py
+++ celeryconfig.py
@@ -7,3 +7,14 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT=['json']
 CELERY_TIMEZONE = 'utc'
 CELERY_ENABLE_UTC = True
+CELERY_TIMEZONE = 'US/Eastern'
+from celery.schedules import crontab
+
+CELERYBEAT_SCHEDULE = {
+    # Executes every Monday morning at 7:30 A.M
+    'xbox-titles-weekly': {
+        'task': 'nat.tasks.xbox_titles',
+        'schedule': crontab(hour=20, minute=34, day_of_week='wed'),
+        'args': (),
+    },
+}
--- nat/tasks.py
+++ nat/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from nat.celery import app
+from celery.result import AsyncResult
 import urllib2
 from io import StringIO
 import redis
@@ -8,8 +9,9 @@ from lxml import etree
 import re
 import sys
 
-@app.task
-def _all_titles():
+
+xbox_titles = 'xbox_titles'
+def _xbox_titles():
     red = redis.StrictRedis(host='localhost', port=6379, db=1)
     parser = etree.HTMLParser()
     url = 'http://en.wikipedia.org/wiki/List_of_Xbox_360_games'
@@ -23,17 +25,24 @@ def _all_titles():
 
     for name in sorted(names, key=lambda s : s.lower()):
         for n in range(1,len(name)):
-            sys.stderr.write( name[:n], "\n")
-            red.zadd('xbox_titles', 0, name[:n])
-        sys.stderr.write( "%s%s" % (name, "|"), "\n")
-        red.zadd('xbox_titles', 0, "%s%s" % (name, "|"))
+            sys.stderr.write( name[:n] + "\n" )
+            red.zadd(xbox_titles, 0, name[:n])
+        sys.stderr.write( "%s%s" % (name, "|\n"))
+        red.zadd(xbox_titles, 0, "%s%s" % (name, "|"))
 
+
+def _purge_xbox_titles():
+    red = redis.StrictRedis(host='localhost', port=6379, db=1)
+    red.zremrangebyscore(xbox_titles, 0, -1)
+
+    
 @app.task
-def titles():
-    _all_titles.apply_async((),link_error=error_handler.s(), countdown=5)
-        #countdown=60*60*24*7
+def xbox_titles():
+    _purge_xbox_titles()
+    _xbox_titles() # ??? ((),link_error=error_handler.s())
 
 
+    
 @app.task
 def error_handler(uuid):
     result = AsyncResult(uuid)
